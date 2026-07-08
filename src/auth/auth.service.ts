a0a94d6b8f9f31f07b64797fa0e8c852fbf605e3
Your Name <you@example.com>
2026-07-08 10:49:34 +0700

    4,6. Implement: account registration and password hash

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(registerDto: RegisterDto) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        username: registerDto.username,
+      },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('User already exists!');
+    }
+
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        username: registerDto.username,
+        password: hashedPassword,
+        name: registerDto.name,
+      },
+      select: {
+        id: true,
+        username: true,
+        name: true,
+        role: true,
+        createdAt: true,
+      },
+    });
+
+    return {
+      message: 'Register successfully',
+      user,
+    };
+  }
+}
